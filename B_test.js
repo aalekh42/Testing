a16a59ad529ec2a369102d24defd640383c9617c
aalekh42 <aalekhpradeep@gmail.com>
2021-02-18 18:45:45 +0530

    Story A100 Solved Merge issue and kept only remote changes

--- B_test.js
+++ B_test.js
@@ -1,8 +1,3 @@
 Branch test file
-<<<<<<< HEAD
-(Made changes in local again)
-(Changed in remote too) HA  
-=======
 (Made changes in local)
 (Changed in remote too)
->>>>>>> ff1aff87eb65d6b8497e19ed07bb927890df0270
